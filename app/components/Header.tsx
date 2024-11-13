c94a649d3c4f117a8bd8e23b5e83c4d2fdc85bd7
srdgz <sandra.rr@icloud.com>
2024-11-13 19:11:15 +0100

    feat: add new font and images

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Platform } from "react-native";
+import { View, Text, Platform, Image } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 interface HeaderProps {
@@ -15,6 +15,11 @@ const Header: React.FC<HeaderProps> = () => {
       className="flex-row items-center justify-center px-4 py-4 bg-gray-800"
       style={{ paddingTop: ios ? top : top + 10 }}
     >
+      <Image
+        source={require("../../assets/images/icon.png")}
+        style={{ width: 24, height: 24, marginRight: 4 }}
+        resizeMode="contain"
+      />
       <Text className="text-2xl text-white font-bold">
         <Text className="text-emerald-500">my</Text>albatross
       </Text>
