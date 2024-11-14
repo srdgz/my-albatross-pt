7ac8808334808d4790fdebf1ab9b7751a5ac144e
srdgz <sandra.rr@icloud.com>
2024-11-14 09:40:50 +0100

    fix: modify loader styles

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,26 +1,12 @@
 import React from "react";
-import { View, ActivityIndicator, StyleSheet } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 
 const Loading: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center absolute inset-0 bg-gray-800">
       <ActivityIndicator size="large" color="#10b981" />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: "#1f2937",
-  },
-});
 
 export default Loading;
