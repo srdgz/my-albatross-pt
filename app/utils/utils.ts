4add685efbfae072febb1de13a99732bc5ddaa5b
srdgz <sandra.rr@icloud.com>
2024-11-14 11:24:11 +0100

    fix: modify export to formatDate function

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,8 +1,6 @@
-const formatDate = (date: string) => {
+export const formatDate = (date: string) => {
   const parsedDate = new Date(date);
   const year = parsedDate.getFullYear();
   const month = parsedDate.getMonth() + 1;
   return `${year}-${month < 10 ? `0${month}` : month}`;
 };
-
-export default formatDate;
