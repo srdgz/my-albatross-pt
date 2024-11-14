6ee5cd63f3d2457494691d3ecf1552c9046991be
srdgz <sandra.rr@icloud.com>
2024-11-14 11:19:48 +0100

    fix: modify export type

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,6 +1,8 @@
-export const formatDate = (date: string) => {
+const formatDate = (date: string) => {
   const parsedDate = new Date(date);
   const year = parsedDate.getFullYear();
   const month = parsedDate.getMonth() + 1;
   return `${year}-${month < 10 ? `0${month}` : month}`;
 };
+
+export default formatDate;
